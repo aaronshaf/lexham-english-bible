c3ec05b3559916f61502c9e8156f8a50592cd8a0
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-12-03 22:26:25 -0700

    Conversion to OSIS book abbreviation

--- scripts/xml_to_json.rb
+++ scripts/xml_to_json.rb
@@ -2,6 +2,74 @@ require 'json'
 
 books = {}
 
+abbreviations = {
+  "Ge" => "Gen",
+  "Ex" => "Exod",
+  "Le" => "Lev",
+  "Nu" => "Num",
+  "Dt" => "Deut",
+  "Jos" => "Josh",
+  "Jud" => "Jude",
+  "Ru" => "Ruth",
+  "1Sa" => "1Sam",
+  "2Sa" => "2Sam",
+  "1Ki" => "1Kgs",
+  "2Ki" => "2Kgs",
+  "1Ch" => "1Chr",
+  "2Ch" => "2Chr",
+  "Ezr" => "Ezra",
+  "Ne" => "Neh",
+  "Es" => "Esth",
+  "Job" => "Job",
+  "Ps" => "Ps",
+  "Pr" => "Prov",
+  "Ec" => "Eccl",
+  "So" => "Song",
+  "Is" => "Isa",
+  "Je" => "Jer",
+  "La" => "Lam",
+  "Eze" => "Ezek",
+  "Da" => "Dan",
+  "Ho" => "Hos",
+  "Joe" => "Joel",
+  "Am" => "Amos",
+  "Ob" => "Obad",
+  "Jon" => "Jonah",
+  "Mic" => "Mic",
+  "Na" => "Nah",
+  "Hab" => "Hab",
+  "Zep" => "Zeph",
+  "Hag" => "Hag",
+  "Zec" => "Zech",
+  "Mal" => "Mal",
+  "Mt" => "Matt",
+  "Mk" => "Mark",
+  "Lk" => "Luke",
+  "Jn" => "John",
+  "Ac" => "Acts",
+  "Ro" => "Rom",
+  "1Co" => "1Cor",
+  "2Co" => "2Cor",
+  "Ga" => "Gal",
+  "Eph" => "Eph",
+  "Php" => "Phil",
+  "Col" => "Col",
+  "1Th" => "1Thess",
+  "2Th" => "2Thess",
+  "1Ti" => "1Tim",
+  "2Ti" => "2Tim",
+  "Tit" => "Titus",
+  "Phm" => "Phlm",
+  "Heb" => "Heb",
+  "Jas" => "Jas",
+  "1Pe" => "1Pet",
+  "2Pe" => "2Pet",
+  "1Jn" => "1John",
+  "2Jn" => "2John",
+  "3Jn" => "3John",
+  "Re" => "Rev"
+}
+
 Dir.glob("../txt/*.txt").each do |file|
   book = ""
   passage = ""
@@ -43,9 +111,11 @@ Dir.glob("../txt/*.txt").each do |file|
       passage = '1:' + passage
     end
 
+    osisAbbreviation = abbreviations[book]
+
     if(!books[book])
       books[book] = {
-        "abbreviation" => book,
+        "abbreviation" => osisAbbreviation,
         "passages" => {}
       }
     end
@@ -59,7 +129,8 @@ Dir.glob("../txt/*.txt").each do |file|
 end
 
 books.each do |book, content|
-  File.open("../json/#{book}.json","w") do |fileToWrite|
+  osisAbbreviation = abbreviations[book]
+  File.open("../json/#{osisAbbreviation}.json","w") do |fileToWrite|
     fileToWrite.write(JSON.pretty_generate(content))
   end
 end
